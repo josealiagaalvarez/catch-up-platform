713217f3eb492dcf06a8f759bd7624115d205989
josealiagaalvarez <jaliagaalvarez@gmail.com>
2025-11-19 11:55:02 -0500

    feat(database): implement AppDbContext with snake case naming convention and created/updated date interceptor

---
+++ CatchUpPlatform/Shared/Domain/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using CatchUpPlatform.Shared.Domain.Infrastructure.Persistence.EFC.Configuration.Extensions;
+using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CatchUpPlatform.Shared.Domain.Infrastructure.Persistence.EFC.Configuration;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        builder.AddCreatedUpdatedInterceptor();
+        base.OnConfiguring(builder);
+    }
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        
+        base.OnModelCreating(builder);
+        builder.useSnakeCaseNamingConvention();
+    }
+}
