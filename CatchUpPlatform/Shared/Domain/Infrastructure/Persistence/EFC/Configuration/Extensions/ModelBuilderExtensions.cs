7d309cbaed36a2b3d8771d7d31ba724f13f33816
josealiagaalvarez <jaliagaalvarez@gmail.com>
2025-11-19 00:17:00 -0500

    feat(namig-conventions): add model builder and string extension methods for snake case naming convention

---
+++ CatchUpPlatform/Shared/Domain/Infrastructure/Persistence/EFC/Configuration/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CatchUpPlatform.Shared.Domain.Infrastructure.Persistence.EFC.Configuration.Extensions;
+
+public static class ModelBuilderExtensions
+{
+    public static void useSnakeCaseNamingConvention(this ModelBuilder builder)
+    {
+        foreach (var entity in builder.Model.GetEntityTypes())
+        {
+            var tableName = entity.GetTableName();
+            if (!string.IsNullOrEmpty(tableName))
+            {
+                entity.SetTableName(tableName.toPlural().ToSnakeCase());
+            }
+
+            foreach (var property in entity.GetProperties())
+            {
+                property.SetColumnName(property.GetColumnName().ToSnakeCase());
+            }
+
+            foreach (var key in entity.GetKeys())
+            {
+                var keyName = key.GetName();
+                if (!string.IsNullOrEmpty(keyName))
+                {
+                    key.SetName(keyName.ToSnakeCase());
+                }
+            }
+
+            foreach (var foreignKey in entity.GetForeignKeys())
+            {
+                var foreignKeyName = foreignKey.GetConstraintName();
+                if (!string.IsNullOrEmpty(foreignKeyName))
+                {
+                    foreignKey.SetConstraintName(foreignKeyName.ToSnakeCase());
+                }
+            }
+
+            foreach (var index in entity.GetIndexes())
+            {
+                var indexName = index.GetDatabaseName();
+                if (!string.IsNullOrEmpty(indexName))
+                {
+                    index.SetDatabaseName(indexName.ToSnakeCase());
+                }
+            }
+        }
+    }
+}
