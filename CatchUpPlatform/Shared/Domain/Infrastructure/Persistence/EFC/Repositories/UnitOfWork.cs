51aebe674c298cd27c4b8147596d6f462e685e87
josealiagaalvarez <jaliagaalvarez@gmail.com>
2025-11-19 22:29:35 -0500

    feat(database): add unit of work implementation for managing database transactions

---
+++ CatchUpPlatform/Shared/Domain/Infrastructure/Persistence/EFC/Repositories/UnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using CatchUpPlatform.Shared.Domain.Infrastructure.Persistence.EFC.Configuration;
+using CatchUpPlatform.Shared.Domain.Repositories;
+
+namespace CatchUpPlatform.Shared.Domain.Infrastructure.Persistence.EFC.Repositories;
+
+public class UnitOfWork(AppDbContext context) : IUnitOfWork
+{
+    
+    
+    public async Task CompleteAsync()
+    {
+        await context.SaveChangesAsync();
+    }
+
+    
+}
