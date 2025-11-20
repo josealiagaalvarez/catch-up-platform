95fed29ccb468fd2c9f57ec736ce0b531e1c1942
josealiagaalvarez <jaliagaalvarez@gmail.com>
2025-11-20 00:42:47 -0500

    feat(favorite-source): add query service for retrieving favorite sources by news api key and source id

---
+++ CatchUpPlatform/News/Application/Internal/QueryServices/FavoriteSourceQueryServices.cs
@@ -0,0 +1,25 @@
+﻿using CatchUpPlatform.News.Domain.Model.Aggregates;
+using CatchUpPlatform.News.Domain.Model.Queries;
+using CatchUpPlatform.News.Domain.Repositories;
+using CatchUpPlatform.Shared.Domain.Services;
+
+namespace CatchUpPlatform.News.Application.Internal.QueryServices;
+
+public class FavoriteSourceQueryServices(
+    IFavoriteSourceRepository favoriteSourceRepositoryrepository) : IFavoriteSourceQueryService
+{
+    public async Task<IEnumerable<FavoriteSource?>> Handle(GetAllFavoriteSourceByNewsApiKeyQuery query)
+    {
+        return await favoriteSourceRepositoryrepository.FindByNewsApiKeyAsync(query.NewsApiKey);
+    }
+
+    public async Task<FavoriteSource?> Handle(GetFavoriteSourceByNewsApiKeyAndSourceIdQuery query)
+    {
+        return await favoriteSourceRepositoryrepository.FindByNewsApiKeyAndSourceIdAsync(query.NewsApiKey, query.SourceId);
+    }
+
+    public async Task<FavoriteSource?> Handle(GetFavoriteSourceByIdQuery query)
+    {
+        return await favoriteSourceRepositoryrepository.FindByIdAsync(query.Id);
+    }
+}
