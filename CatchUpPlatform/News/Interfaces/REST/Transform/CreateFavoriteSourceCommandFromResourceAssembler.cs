745ee0da3f27a57fdf755bfc935eebfc7b322e36
josealiagaalvarez <jaliagaalvarez@gmail.com>
2025-11-21 01:13:25 -0500

    feat(favorite-source): add assemblers for converting between resources and commands

---
+++ CatchUpPlatform/News/Interfaces/REST/Transform/CreateFavoriteSourceCommandFromResourceAssembler.cs
@@ -0,0 +1,12 @@
+﻿using CatchUpPlatform.News.Domain.Model.Commads;
+using CatchUpPlatform.News.Interfaces.REST.Resources;
+
+namespace CatchUpPlatform.News.Interfaces.REST.Transform;
+
+public static class CreateFavoriteSourceCommandFromResourceAssembler
+{
+    public static CreateFavoriteSourceCommand ToCommandFromResource(CreateFavoriteSourceResource resource)
+    {
+        return new CreateFavoriteSourceCommand(resource.NewsApiKey, resource.SourceId);
+    }
+}
---
+++ CatchUpPlatform/News/Interfaces/REST/Transform/FavoriteSourceResourceFromEntityAssembler.cs
@@ -0,0 +1,13 @@
+﻿using CatchUpPlatform.News.Domain.Model.Aggregates;
+
+using CatchUpPlatform.News.Interfaces.REST.Resources;
+
+namespace CatchUpPlatform.News.Interfaces.REST.Transform;
+
+public static class FavoriteSourceResourceFromEntityAssembler
+{
+    public static FavoriteSourceResource ToResourceFromEntity(FavoriteSource entity)
+    {
+        return new FavoriteSourceResource(entity.Id, entity.NewsApiKey, entity.SourceId);
+    }
+}
